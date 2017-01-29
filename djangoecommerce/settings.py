4b42895d6be2431f58da618e05806c13b90c5613
supcrawler <eosbornguy@gmail.com>
2017-01-29 15:21:57 -0200

    envio email

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -143,7 +143,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mailcontactadm@gmail.com'
-EMAIL_HOST_PASSWORD = 'contact123'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'mailcontactadm@gmail.com'
 
 try:
