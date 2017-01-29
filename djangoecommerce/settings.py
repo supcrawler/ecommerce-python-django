6792982b22713bc03070950761031ebad78bd38b
supcrawler <eosbornguy@gmail.com>
2017-01-29 13:45:41 -0200

    envio email

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -139,10 +139,12 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # E-mail
-EMAIL_HOST = ''
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = 'admin@djangoecommerce.com'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mailcontactadm@gmail.com'
+EMAIL_HOST_PASSWORD = 'contact123'
+DEFAULT_FROM_EMAIL = 'mailcontactadm@gmail.com'
 
 try:
     from .local_settings import *
